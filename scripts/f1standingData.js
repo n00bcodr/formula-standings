a239a6b39c6f265652ee7a7fd0ff24f848ded99f
GitHub Action <action@github.com>
2023-11-11 00:27:42 +0000

    auto-update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -192,4 +192,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='05/11/2023 23:31:10 UTC';
+document.getElementById('lastup').innerHTML='11/11/2023 00:27:27 UTC';
